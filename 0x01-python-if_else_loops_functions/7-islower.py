1b98891ab4b7194efbb361864af43e057de7f24d
Toby16 <bolajibaasit14@gmail.com>
2023-01-03 14:50:54 -0500

    Python script | checks for lower and uppercase | ...

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(97, 123):
-        if c in ord(i):
+    for i in "abcdefghijklmnopqrstuvwxyz":
+        if ord(c) == ord(i):
             return True
-    for i in range(65, 91):
-        if c in ord(i):
+    for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        if ord(c) in ord(i):
             return False
