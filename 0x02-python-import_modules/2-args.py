f02a5bcc16e9de601148ff4bab70527906a7f6d9
Toby16 <bolajibaasit14@gmail.com>
2023-01-05 16:05:29 -0500

    Python program | prints number of and list of its arguments | changed sys.argv to sys.argv[1:] on line 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     j = 0
-    print("{:d} arguments:".format(len(sys.argv)))
+    print("{:d} arguments:".format(len(sys.argv[1:])))
     for i in sys.argv[1:]:
         j += 1
         if j != (len(sys.argv)):
