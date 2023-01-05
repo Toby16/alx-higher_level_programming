f5562fc508ffc3edfbc685728209379117b74174
Toby16 <bolajibaasit14@gmail.com>
2023-01-05 11:36:14 -0500

    Python program | imports a function from a file, prints out result of operation carried out

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+
+if __name__ = "__main__":
+    import sys
+    add(int(sys.argv[1]))
