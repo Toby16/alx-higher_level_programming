cf34e9d079621c3be63c456c45efdacb5d3f8c2e
Toby16 <bolajibaasit14@gmail.com>
2023-01-05 16:12:19 -0500

    Python program | prints number of and list of its arguments | no commit message

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     import sys
     j = 0
-    print("{:d} arguments:".format(len(sys.argv[1:])))
-    for i in sys.argv[1:]:
-        j += 1
-        print("{:d}: {:s}".format(j, i))
+    if len(sys.argv[1:]) == 0:
+        print("{:d} arguments.".format(len(sys.argv[1:])))
+    else:
+        print("{:d} arguments:".format(len(sys.argv[1:])))
+        for i in sys.argv[1:]:
+            j += 1
+            print("{:d}: {:s}".format(j, i))
