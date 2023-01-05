2da3c92af4c0bf78f63f862bea36ed1d70224f4f
Toby16 <bolajibaasit14@gmail.com>
2023-01-05 16:18:05 -0500

    Python program | prints number of and list of its arguments | refactored code | can't remember changes made

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     j = 0
     if len(sys.argv[1:]) == 0:
         print("{:d} arguments.".format(len(sys.argv[1:])))
+    elif len(sys.argv[1:]) == 1:
+        print("{:d} argument:".format(len(sys.argv[1:])))
+        for i in sys.argv[1:]:
+            j += 1
+            print("{:d}: {:s}".format(j, i))
     else:
         print("{:d} arguments:".format(len(sys.argv[1:])))
         for i in sys.argv[1:]:
