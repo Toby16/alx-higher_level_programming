f9b2a70b847a570df10dbdeb2f27d277334f54dc
Toby16 <bolajibaasit14@gmail.com>
2023-01-09 10:49:38 -0500

    Python program |  function that replaces an element in a list at a specific position without modifying the original list | removed "isinstance()" function and trailing while space

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if isinstance(my_list, list):
-        if idx < 0:
-            return my_list
-        elif idx >= len(my_list):
-            return my_list
-        else:
-            new_l = my_list.copy()
-            new_l[idx] = element
-            return new_l
-
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        new_l = my_list.copy()
+        new_l[idx] = element
+        return new_l
