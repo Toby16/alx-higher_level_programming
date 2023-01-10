6bbc9cc3d8b542698248bedc9114662ab6b6c3df
Toby16 <bolajibaasit14@gmail.com>
2023-01-09 19:07:17 -0500

    C program | function that checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+	{
+        	return true;
+	}
+
+	/*find the middle of the list*/
+	struct ListNode *slow = head;
+	struct ListNode *fast = head;
+	while (fast->next != NULL && fast->next->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+    /*reverse the second half of the list*/
+	struct ListNode *prev = NULL;
+	struct ListNode *curr = slow->next;
+	while (curr != NULL)
+	{
+		struct ListNode *temp = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = temp;
+	}
+	slow->next = prev;
+
+    /*compare the first and second halves of the list*/
+	while (head != slow)
+	{
+		if (head->val != slow->val)
+		{
+			return 0;
+		}
+		head = head->next;
+		slow = slow->next;
+	}
+	return 1;
+}
