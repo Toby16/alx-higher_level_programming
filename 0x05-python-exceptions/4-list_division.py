db054b1d04049d90b7423e735f473ac832c2ac0c
Toby16 <bolajibaasit14@gmail.com>
2023-01-23 17:40:05 -0500

    function that divides element by element 2 lists | changed 'while' loop to 'for' loop | Only God knows what i've written here

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,22 +2,18 @@
 def list_division(my_list_1, my_list_2, list_length):
     val_lst = []
     i = 0
-    while i < list_length:
+    for i in range(list_length):
         try:
             div_val = my_list_1[i]/my_list_2[i]
-            i += 1
         except (TypeError):
             print("wrong type")
             div_val = 0
-            i += 1
         except ZeroDivisionError:
             print("division by zero")
             div_val = 0
-            i += 1
         except IndexError:
             print("out of range")
             div_val = 0
-            i += 1
         finally:
             val_lst.append(div_val)
     return val_lst
