9a203eb28bc4eebd565897100482405e6250851a
Toby16 <bolajibaasit14@gmail.com>
2023-01-23 19:19:16 -0500

    <..> | no commit because i'm tired

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             div_val = 0
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             div_val = 0
         except IndexError:
             print("out of range")
