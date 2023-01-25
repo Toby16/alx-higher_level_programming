2ad29c6bab34de3cb83137763edf7ef5a2e44024
Toby16 <bolajibaasit14@gmail.com>
2023-01-24 20:45:05 -0500

    Coordinates of a square | corrected type errors | corrected code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -98,11 +98,8 @@ class Square:
         if (self.__size == 0):
             print("")
             return
-        for i in range(self.__position[1]):
-            print("")
-        for j in range(self.__size):
-            for x in range(self.__position[0]):
-                print(" ", end="")
-            for y in range(self.__size):
-                print("#", end="")
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
             print("")
