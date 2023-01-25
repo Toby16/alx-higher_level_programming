badf80a293afa393f3101478453b9621e993df8c
Toby16 <bolajibaasit14@gmail.com>
2023-01-24 20:12:44 -0500

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""
+'Square' class that defines a square
+"""
+
+
+class Square:
+    """
+    defines a square
+    Attributes:
+        size: size of square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        init method for class 'Square'
+        Args:
+            size: size of square
+            position: position of square on cartesian plane
+        """
+        self.__size = size
+        self.__position = position
+
+        if not (isinstance(self.__size, int)):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+        if ((not isinstance(self.__position[0], int)) or
+                (not isinstance(self.__position[1], int))):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not isinstance(self.__position, tuple) or
+                (self.__position[0] < 0) or (self.__position[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def size(self):
+        """
+        method of class Square
+        Returns:
+            size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        method to assign value to size
+        Args:
+            value: value for size
+        """
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """
+        method to return a tuple of two positive integers
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """
+        method to assign value to position
+        Args:
+            value: tuple value for position
+        """
+        if ((not isinstance(self.__position[0], int)) or
+                (not isinstance(self.__position[1], int))):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not isinstance(self.__position, tuple) or
+                (self.__position[0] < 0) or (self.__position[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__property = value
+
+    def area(self):
+        """
+        method to return area of square
+
+        Returns:
+            area of Square object
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            for x in range(self.__position[0]):
+                print(" ", end="")
+            for y in range(self.__size):
+                print("#", end="")
+            print()
