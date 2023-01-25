ac07e5d5b9fc3bbe692e89aa1af12cb5eef56723
Toby16 <bolajibaasit14@gmail.com>
2023-01-24 20:33:13 -0500

    Coordinates of a square | corrected type errors | corrected code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -94,7 +94,7 @@ class Square:
         method to print '#' to form square
         """
         if (self.__size == 0):
-            print("\n")
+            print("")
             return
         for i in range(self.__position[1]):
             print()
