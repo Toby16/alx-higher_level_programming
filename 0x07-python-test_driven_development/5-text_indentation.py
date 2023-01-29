c46b44d54dd5c051252422c408b0b1f521b37ee5
Toby16 <bolajibaasit14@gmail.com>
2023-01-29 07:00:49 -0500

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Text indentation
+Module that contains function to output text
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        """
+        checks if text is of type <class 'int'>
+        if False, TypeError is raised
+        """
+        raise TypeError("text must be a string")
+    else:
+        str_val = ""
+        val = ""
+        for i in range(len(text)):
+            if (text[i - 1] in [".", "?", ":"]):
+                if (text[i] not in [".", "?", ":"]):
+                    val = "\n\n"
+            else:
+                val = text[i]
+            str_val += val
+    print(str_val)
