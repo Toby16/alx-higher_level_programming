1011553080ca0ec411dd6f586036965e2578321b
Toby16 <bolajibaasit14@gmail.com>
2023-01-29 05:13:56 -0500

    Say my name | outputs formated string | made file executable

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Say my name
+Module comtaining function that prints name
+"""
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        """
+        check if first_name is of type <class 'str'>
+        """
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        """
+        check is last_name is of type <class 'str'>
+        """
+        raise TypeError("last_name must be a string")
+    else:
+        """
+        prints string to stdout
+        """
+        print("My name is {:s} {:s}".format(first_name, last_name));
