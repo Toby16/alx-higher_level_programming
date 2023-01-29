8fda4cd71361e01f860ee25e083469f3ed477df5
Toby16 <bolajibaasit14@gmail.com>
2023-01-29 07:05:36 -0500

    Say my name | fixed pycodestyle errors

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@ Say my name
 Module comtaining function that prints name
 """
 
+
 def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         """
@@ -19,4 +20,4 @@ def say_my_name(first_name, last_name=""):
         """
         prints string to stdout
         """
-        print("My name is {:s} {:s}".format(first_name, last_name));
+        print("My name is {:s} {:s}".format(first_name, last_name))
