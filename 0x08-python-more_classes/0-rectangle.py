2cc9a528b230fcbe9b533bdbba0567177fb5a099
Toby16 <bolajibaasit14@gmail.com>
2023-01-30 05:32:00 -0500

    empty class 'Rectangle' | <...>

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Simple Rectangle
+contains class that defines a rectangle
+No module imported
+"""
+
+
+class Rectangle:
+    """
+    class that defines a rectangle
+    Argument:
+        No argument(s)
+    """
+    pass
