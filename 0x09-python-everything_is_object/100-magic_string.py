ad38f84803fcb7ea1e7b166161c575831229e789
Toby16 <bolajibaasit14@gmail.com>
2023-01-31 20:05:19 -0500

    raises name exception with message | fixed requirement error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-#pythonic
-"""
-
-
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
