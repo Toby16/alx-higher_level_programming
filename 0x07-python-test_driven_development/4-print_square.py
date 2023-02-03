ebbeb5e5df1da7a72488d9e2bafab58585c9a9b4
Toby16 <bolajibaasit14@gmail.com>
2023-02-03 18:59:16 -0500

    print square | prints a square with char '#'

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,7 +15,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(size):
-        for j in range(size):
-            print('#', end="")
-        print()
+    else:
+        for i in range(size):
+            [print("#", end="") for j in range(size)]
+            print("")
