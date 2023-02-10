f398fdce0e51726be160a999719bbc19b73d1633
Toby16 <bolajibaasit14@gmail.com>
2023-02-10 12:46:44 -0500

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,3 +111,33 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        """
+        method to return area of rectangle object
+        Returns:
+            width * height
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        method to print the Rectangle instance with the '#' character in stdout
+        """
+        i = 0
+        while i < self.__height:
+            j = 0
+            while j < self.__width:
+                print("#", end="")
+                j += 1
+            print()
+            i += 1
+
+    def __str__(self):
+        """
+        overriding str method to return:
+            '[Rectangle] (<id>) <x>/<y> - <width>/<height>' to stdout
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
