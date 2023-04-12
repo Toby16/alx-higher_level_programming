5d255507397b9e3d03341779c86b87089a0b9499
Toby16 <bolajibaasit14@gmail.com>
2023-04-11 21:42:18 -0400

    Second biggest! | Report:  script that searches the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const argCount = argv.length - 2;
+
+if (argCount <= 1) {
+  console.log(0);
+} else {
+  const lst = [];
+  for (let i = 0; i < argCount; i += 1) {
+    // You can assume all arguments can be converted to integer
+    lst[i] = Number(argv[i + 2]);
+  }
+  lst.sort();
+  console.log(lst[4]);
+}
