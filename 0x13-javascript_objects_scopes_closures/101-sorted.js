286b848973d5ae6c6fecda606401ff9bde6a10ee
Toby16 <bolajibaasit14@gmail.com>
2023-04-12 21:03:05 -0400

    Sorted occurences | Report: script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const importedVal = require('./101-data.js');
+const dictVal = importedVal.dict;
+
+const newDict = {};
+for (const key in dictVal) {
+  if (newDict[dictVal[key]] === undefined) {
+    newDict[dictVal[key]] = [];
+  }
+  newDict[dictVal[key]].push(key);
+}
+
+console.log(dictVal);
+console.log(newDict);
