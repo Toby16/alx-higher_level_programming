3e7aaa85388e1a47a5a620e747698b6e061cbf53
Toby16 <bolajibaasit14@gmail.com>
2023-04-17 22:37:50 -0400

    Cities in state | Report: added comments

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+"""
+This script retrieves all cities
+in a state from the database and prints them out
+in the format "<state name>: (<city id>) <city name>".
+
+It uses the City and State models defined in model_city.py
+and model_state.py respectively, and connects to the
+database using the credentials passed as command line arguments.
+
+Usage: ./14-model_city_fetch_by_state.py <db_username> <db_password> <db_name>
+"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -16,6 +28,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session()
 
+    # Query the database for all cities and their associated state
     for cities, state in session.query(City, State).filter(
             City.state_id == State.id).order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, cities.id, cities.name))
