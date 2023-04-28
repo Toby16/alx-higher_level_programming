4e063a3f72c9ceff3d621fcd474fea8856c33dc5
Toby16 <bolajibaasit14@gmail.com>
2023-04-28 19:21:14 -0400

    What's my status? #0 | Report: used tabs '\t' escape sequence

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,13 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    this_page = response.read()
+    with urllib.request.urlopen(url) as response:
+        this_page = response.read()
 
-print("Body response:")
-print("    - type:", type(this_page))
-print("    - content:", this_page)
-print("    - utf8 content:", this_page.decode("utf-8"))
+    print("Body response:")
+    print("\t- type:", type(this_page))
+    print("\t- content:", this_page)
+    print("\t- utf8 content:", this_page.decode("utf-8"))
