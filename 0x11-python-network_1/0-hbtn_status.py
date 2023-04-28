6bb59585c9f1794a1cc36a2b692d12dd551465f7
Toby16 <bolajibaasit14@gmail.com>
2023-04-28 19:13:33 -0400

    What's my status? #0 | Report: file is executable and pycodestyle compliant

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    this_page = response.read()
+
+print("Body response:")
+print("     - type:", type(this_page))
+print("     - content:", this_page)
+print("     - utf8 content:", this_page.decode("utf-8"))
