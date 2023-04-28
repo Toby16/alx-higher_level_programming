e1669ded4d68e02a2a0b8d53d24afad23d78e3bf
Toby16 <bolajibaasit14@gmail.com>
2023-04-27 21:13:41 -0400

    cURL POST parameters | Report: no commit message

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -d "param1=value1" -d "param2=value2"
+curl -s -X POST "$1" -d "email: test@gmail.com&subject: I will always be here for PLD"
