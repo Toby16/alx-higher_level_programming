99789739658e97c0820c02cb2afe4ecbf66abd6d
Toby16 <bolajibaasit14@gmail.com>
2023-04-27 20:30:39 -0400

    cURL to the end | Report: 200~Display only body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -X GET -s -w "%{http_code}" "$1" -o /dev/null | grep -q 200 && curl -s "$1"
+curl "$1" -sL
