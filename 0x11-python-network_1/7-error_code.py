6bb6c53fd45c4c34ae302633c59999a825ee11fc
Toby16 <bolajibaasit14@gmail.com>
2023-04-28 21:05:46 -0400

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL
+and displays the body of the response
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = request.get(url)
+
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)
