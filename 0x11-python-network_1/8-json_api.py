4094f487211a6a44e54dae0cec3368b4b0d913d8
Toby16 <bolajibaasit14@gmail.com>
2023-04-28 21:31:30 -0400

    Search API | Report: no commit message

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = ""
+
+    if sys.argv[1]:
+        q = sys.argv[1]
+    data = {"q": q}
+
+    r = requests.post(url, data=data)
+    try:
+        json_r = r.json()
+        if json_r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_r["id"], json_r["name"]))
+    except TypeError, ValueError:
+        print("Not a valid JSON")
