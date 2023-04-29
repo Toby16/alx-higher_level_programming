bce0f845395c081574fa5e89863328c5cdba8474
Toby16 <bolajibaasit14@gmail.com>
2023-04-28 22:04:23 -0400

    My GitHub! | Report:

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    auth = requests.auth.HTTPBasicAuth(username, password)
+    response = requests.get(url, auth=auth)
+
+    user_data = response.json()
+    user_id = user_data.get("id")
+    print(user_id)
+    print(response.json())
