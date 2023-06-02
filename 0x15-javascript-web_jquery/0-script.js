0c1aed268490b526ccfec60d09c7b61e450acd77
Toby16 <bolajibaasit14@gmail.com>
2023-06-02 06:13:34 -0400

    No JQuery | Report: Semistandard complaint and executable

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* select <header> element */
+const headerVar = document.querySelector('header');
+
+/* Change the text color of the selected element */
+headerVar.style.color = '#FF0000';
